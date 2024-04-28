f9078e55b0c356dc3786e968e2b7ad08ef10c5bf
lukebodm <116473896+lukebodm@users.noreply.github.com>
2024-04-28 17:41:02 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,32 +1,24 @@
-{ callPackage
-, lib
-, stdenv
-, fetchurl
-, nixos
-, testers
-, hello
-}:
+with import <nixpkgs> {}; stdenv.mkDerivation {
+  name = "hello";
 
-stdenv.mkDerivation (finalAttrs: {
-  pname = "hello-nix";
-  version = "2.12.1";
+  # Source Code
+  # See: https://nixos.org/nixpkgs/manual/#ssec-unpack-phase
+  src = ./src;
 
-  src = fetchurl {
-    url = "mirror://gnu/hello/hello-${finalAttrs.version}.tar.gz";
-    sha256 = "sha256-jZkUKv2SV28wsM18tCqNxoCZmLxdYH2Idh9RLibH2yA=";
-  };
+  # Dependencies
+  # See: https://nixos.org/nixpkgs/manual/#ssec-stdenv-dependencies
+  buildInputs = [ coreutils gcc ];
 
-  meta = with lib; {
-    description = "Just testing my-own-nixpkgs";
-    longDescription = ''
-      GNU Hello is a program that prints "Hello, world!" when you run it.
-      It is fully customizable.
-    '';
-    homepage = "https://www.gnu.org/software/hello/manual/";
-    changelog = "https://git.savannah.gnu.org/cgit/hello.git/plain/NEWS?h=v${finalAttrs.version}";
-    license = licenses.gpl3Plus;
-    maintainers = [ maintainers.eelco ];
-    mainProgram = "hello";
-    platforms = platforms.all;
-  };
-})
+  # Build Phases
+  # See: https://nixos.org/nixpkgs/manual/#sec-stdenv-phases
+  configurePhase = ''
+    declare -xp
+  '';
+  buildPhase = ''
+    gcc "$src/hello.c" -o ./hello
+  '';
+  installPhase = ''
+    mkdir -p "$out/bin"
+    cp ./hello "$out/bin/"
+  '';
+}
